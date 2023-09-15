11b9bbc2b6a03bbffb21735c55a56e2c6e942b34
Gruszek <105386680+GGruszek@users.noreply.github.com>
2023-09-15 14:51:08 +0200

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿namespace mobilnaGruszka;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+        string mail1 = mail.Text;
+        string check = "@";
+       
+		if(mail1.Contains(check))
+		{
+            if (string.IsNullOrEmpty(pass1.Text) || string.IsNullOrEmpty(pass2.Text))
+            {
+                result1.Text = "Proszę podać hasło.";
+            }
+            else
+            {
+                if (pass1.Text == pass2.Text)
+                {
+                    result1.Text = $"Witaj {mail1}";
+                }
+                else
+                {
+                    result1.Text = "Hasła się różnią.";
+                }
+            }
+        }
+        else
+        {
+            result1.Text = "E-mail jest niepoprawny.";
+        }
+		
+	}
+
+    void OnSliderValueChanged(object sender, ValueChangedEventArgs args)
+    {
+        double value = args.NewValue;
+        Convert.ToInt32(value);
+        slideValue.Text = String.Format("The Slider value is {0}", value);
+    }
+}
+
